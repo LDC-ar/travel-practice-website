1270246eb51815d89025e3ce3bb1a8d99fc8e944
LeandroDC <leandrocastagno@hotmail.com>
2023-09-25 20:07:54 -0300

    Completed reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,6 @@
 import "../styles/styles.css"
 import MobileMenu from "./modules/MobileMenu"
+import RevealOnScroll from "./modules/RevealOnScroll"
 
 if (module.hot) {
   module.hot.accept()
@@ -7,3 +8,5 @@ if (module.hot) {
 
 /* CODE BELOW */
 let mobileMenu = new MobileMenu()
+new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
+new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
