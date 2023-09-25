bfdc89e2b08cde32c9d8a1694779472fb818ade9
LeandroDC <leandrocastagno@hotmail.com>
2023-09-25 19:08:42 -0300

    Completed menu mobile

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,6 +1,19 @@
 class MobileMenu {
   constructor() {
-    alert("Testing from mobile")
+    this.siteHeader = document.querySelector(".site-header")
+    this.menuIcon = document.querySelector(".site-header__menu-icon")
+    this.menuContent = document.querySelector(".site-header__menu-content")
+    this.events()
+  }
+
+  events() {
+    this.menuIcon.addEventListener('click', () => this.toggleMenu())
+  }
+
+  toggleMenu() {
+    this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+    this.siteHeader.classList.toggle("site-header--is-expanded")
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
   }
 }
 
